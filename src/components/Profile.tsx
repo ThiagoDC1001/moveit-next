5d86c903dfb53cddf27991321d1c0d9c1302de91
Thiago Della Coletta <cmsthiagoazevedo@gmail.com>
2021-03-18 16:27:08 -0300

    Quarto fia completo, testando GitKraken

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/thiagodc1001.png" alt="ThiagoDC" />
+      <div>
+        <strong>Thiago Coletta</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
